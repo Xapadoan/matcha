29ad256f3712a765266a4d8090e49e5a5117eee1
Hollyol <HollyolGamu@hotmail.com>
2019-07-25 15:23:26 +0200

    Retrieve user's post data and delete unwanted middleware (body-parser | not essential)

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # matcha
 42 Projects
 
+For this version, trying to get user creation work
+
 ## JSON Files
 
 Les fichiers de config sont les .json.
--- serveur.js
+++ serveur.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var settings = require("./server_settings.json");
 
+app.use(express.urlencoded({extended: true}));
+
 app.get('/', (req, res) => {
     res.render('index.ejs');
 });
@@ -12,6 +14,19 @@ app.get('/login', (req, res) => {
 
 app.get('/signup', (req, res) => {
     res.render('signup.ejs');
-})
+});
+
+app.post('/signup', (req, res) => {
+    let username = req.body.Username;
+    let password = req.body.Password;
+    let mail = req.body.Mail;
+    let token = req.body.Csrf;
+    res.render('signup_step1.ejs', {
+        username: username,
+        password: password,
+        mail: mail,
+        token: token
+    });
+});
 
 app.listen(settings['port']);

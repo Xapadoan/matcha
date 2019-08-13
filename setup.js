45909499ee31e2c2a093104779f80da2822c5944
xapadoan <xapadoan@e1r12p7.42.fr>
2019-08-13 17:46:51 +0200

    add userextended form

--- setup.js
+++ setup.js
@@ -1,4 +1,4 @@
-var mysql	= require('mysql');
+var mysql = require('mysql');
 var data = require('./database.json');
 
 var connection = mysql.createConnection({
@@ -16,5 +16,5 @@ connection.connect((err) => {
 connection.query('CREATE DATABASE IF NOT EXISTS ' + data['name']);
 connection.query('USE ' + data['name']);
 connection.query('CREATE TABLE IF NOT EXISTS users (id INT(9) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL, username VARCHAR(100) NOT NULL, lastname VARCHAR(100) NOT NULL, firstname VARCHAR(100) NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL)');
-connection.query('CREATE TABLE IF NOT EXISTS users_extended (id INT(9) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL, INT user NOT NULL, gender INT NOT NULL DEFAULT 0, orientation VARCHAR(50) NOT NULL DEFAULT Hetero, age INT NOT NULL DEFAULT 18, bio TEXT(500), interests VARCHAR(255))');
+connection.query('CREATE TABLE IF NOT EXISTS users_extended (id INT(9) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL, user INT NOT NULL, gender INT NOT NULL DEFAULT 0, orientation VARCHAR(50) NOT NULL DEFAULT Hetero, age INT NOT NULL DEFAULT 18, bio TEXT(500), interests VARCHAR(255))');
 connection.end();

8dabd10e11f0cbce643c008477b6541750d9564e
xapadoan <xapadoan@e1r12p5.42.fr>
2019-08-29 13:29:15 +0200

    handle interrests

--- memberManager.js
+++ memberManager.js
@@ -291,7 +291,7 @@ module.exports = {
 		return (new Promise((resolve, reject) => {
 			//get member id
 			let id;
-			connection.query('SELECT u.id, e.user FROM matcha.users u LEFT JOIN matcha.users_extended e ON matcha.users.id = e.user WHERE username = ?', [
+			connection.query('SELECT u.id, e.user FROM matcha.users u LEFT JOIN matcha.users_extended e ON u.id = e.user AND u.username = ?', [
 				username
 			], (err, results) => {
 				if (err) {

e0522001d3f938569528e356523ee010b656f471
xapadoan <xapadoan@e1r10p10.42.fr>
2019-08-30 15:44:18 +0200

    start geolocation

---
+++ testneutrino.js
@@ -0,0 +1,25 @@
+var request = require('request');
+var serv = require('./server_settings.json');
+
+var params = {
+    'user-id': serv.neutrino_id,
+    'api-key': serv.neutrino,
+    'latitude': 48.887398399999995,
+    'longitude': 2.3126016,
+    'language-code': 'fr',
+    'zoom': 'city'
+};
+
+request.post(
+    'https://neutrinoapi.com/geocode-reverse',
+    {form: params},
+    function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+          var result = JSON.parse(body);
+          console.log(result['country']);
+          console.log(result['country-code']);
+          console.log(result['region']);
+          console.log(result['city']);
+      }
+    }
+);

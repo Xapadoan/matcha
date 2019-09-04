252137e9cfe49161d85907107ee8212e5417b9a0
xapadoan <xapadoan@e1r6p21.42.fr>
2019-09-04 16:51:16 +0200

    generateFakes

--- generateFakes.js
+++ generateFakes.js
@@ -54,7 +54,7 @@ function generateFemales() {
         i2 = body.indexOf('</h3>', i1);
         names = body.substring(i1, i2);
         i1 = body.indexOf('<a id="geo" href="javascript:void(0)">');
-        i1 = body.indexOf('>', i1);
+        i1 = body.indexOf('>', i1) + 1;
         i2 = body.indexOf('</a>', i1);
         geo = body.substring(i1, i2);
         console.log('Name : ' + names);

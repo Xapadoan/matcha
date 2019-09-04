38348110bf20917f6e71c1f0f3879c713cff3648
xapadoan <xapadoan@e1r6p21.42.fr>
2019-09-04 15:51:41 +0200

    reupload README

--- generateFakes.js
+++ generateFakes.js
@@ -50,9 +50,9 @@ function generateFemales() {
         //              <h3>Firstname Lastname</h3>
         //          </div>
         //          <a id='geo' href='...'>lat lng</a>
-        name_regex = new RegExp('<div class="address">(.*?)</div>', 'g');
+        name_regex = new RegExp('<div class="address">(.*?)<\/div>', 'g');
         match = name_regex.exec(body);
-        geo_regex = new RegExp('<a id="geo" href="javascript:void(0)">(.*?)</a>', 'g');
+        geo_regex = new RegExp('<a id="geo" href="javascript:void(0)">(.*?)<\/a>', 'g');
         geo = geo_regex.exec(body);
         console.log('Name : ' + match);
         console.log('Geo : ' + geo);

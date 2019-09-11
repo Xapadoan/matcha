6803871d82fa828765a8652e3d0d0b91f80433e9
xapadoan <xapadoan@e1r6p19.42.fr>
2019-09-11 14:55:14 +0200

    Fix extended user

--- memberManager.js
+++ memberManager.js
@@ -342,6 +342,7 @@ module.exports = {
 		return (new Promise((resolve, reject) => {
 			//get user extended profile
 			this.getUserExtended(username).then((result) => {
+				console.log(result);
 				if (result == false) {
 					console.log('create');
 					//extended profile doesn't exists, we have to create it

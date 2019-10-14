7ef63f6d28a90721c55256c52ab9b1ccd039553b
Hollyol <HollyolGamu@hotmail.com>
2019-10-14 18:46:58 +0200

    handle likes, unlikes, dislikes, block

--- memberManager.js
+++ memberManager.js
@@ -732,6 +732,7 @@ module.exports = {
 						console.log('Failed to register dislike:\n' + err.stack);
 						reject('Failed to register dislike');
 					} else {
+						console.log('unlike')
 						this.unlike(disliker, dislikedid).then((result) => {
 							resolve(result)
 						}).catch((reason) => {

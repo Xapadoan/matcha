431d718928c4bf01a8e49b72fd6ed97a8b458f4c
Hollyol <HollyolGamu@hotmail.com>
2019-10-15 15:46:53 +0200

    Count dislikes

--- memberManager.js
+++ memberManager.js
@@ -972,7 +972,7 @@ module.exports = {
 			query = 'SELECT u.id, u.firstname, u.lastname, u.fruit, e.age, e.gender, e.bio, i.image1 FROM matcha.users u INNER JOIN matcha.users_extended e ON u.id = e.user INNER JOIN matcha.users_images i ON u.id = i.user INNER JOIN matcha.users_interests n ON u.id = n.user'
 			query_values = [fetcher.username];
 			if (typeof options.allow_dislikes != 'undefined' && options.allow_dislikes != true) {
-				query += ' LEFT JOIN matcha.users_dislikes d ON (u.id <> d.disliked OR d.disliker <> (SELECT id FROM matcha.users WHERE username = ?) OR d.id IS NULL)';
+				query += ' RIGHT JOIN matcha.users_dislikes d ON (u.id <> d.disliked OR d.disliker <> (SELECT id FROM matcha.users WHERE username = ?) OR d.id IS NULL)';
 				query_values.push(fetcher.username);
 			}
 			query += ' WHERE u.username <> ?';

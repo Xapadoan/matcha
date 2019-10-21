c6f6073cf2ee3d544e23c86bd92f2f04b1ac7a39
Hollyol <HollyolGamu@hotmail.com>
2019-10-21 16:13:40 +0200

    Rewrite delete_user

--- memberManager.js
+++ memberManager.js
@@ -916,7 +916,7 @@ module.exports = {
 							} else if (res != true) {
 								resolve('Le pseudo et le mot de pass de correspondent pas');
 							} else {
-								let query = 'DELETE matcha.users, matcha.users_extended, matcha.users_images, matcha.users_interests FROM matcha.users';
+								let query = 'DELETE u, matcha.users_extended, matcha.users_images, matcha.users_interests FROM matcha.users AS u';
 								query += ' FULL OUTER JOIN matcha.users_extended ON matcha.users.id = matcha.users_extended.user';
 								query += ' FULL OUTER JOIN matcha.users_images ON matcha.users.id = matcha.users_images.user';
 								query += ' FULL OUTER JOIN matcha.users_interests ON matcha.users.id = matcha.users_interests.user';
@@ -925,7 +925,7 @@ module.exports = {
 								//query += ' LEFT JOIN matcha.users_blocks ON (matcha.users.id = matcha.users_blocks.blocker OR matcha.users.id = matha.users_blocks.blocked)'
 								//query += ' LEFT JOIN matcha.users_visits ON (matcha.users.id = matcha.users_visits.visitor OR matcha.users.id = matha.users_visits.visited)'
 								//query += ' LEFT JOIN matcha.users_reports ON matcha.users.id = matcha.users_reports.reported';
-								//query += ' WHERE matcha.users.username = ?';
+								query += ' WHERE u.username = ?';
 								connection.query(query, [
 									username
 								], (err) => {

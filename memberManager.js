e2f3025e32b1b9201474acf20abbaf9e7d23ea16
Hollyol <HollyolGamu@hotmail.com>
2019-10-21 16:25:01 +0200

    Rewrite delete_user

--- memberManager.js
+++ memberManager.js
@@ -919,12 +919,12 @@ module.exports = {
 								let query = 'DELETE matcha.users, matcha.users_extended FROM matcha.users';
 								query += ' LEFT JOIN matcha.users_extended ON matcha.users.id = matcha.users_extended.user';
 								query += ' LEFT JOIN matcha.users_images ON matcha.users.id = matcha.users_images.user';
-								//query += ' FULL OUTER JOIN matcha.users_interests ON matcha.users.id = matcha.users_interests.user';
-								//query += ' LEFT JOIN matcha.users_likes ON (matcha.users.id = matcha.users_likes.liker OR matcha.users.id = matha.users_likes.liked)'
-								//query += ' LEFT JOIN matcha.users_dislikes ON (matcha.users.id = matcha.users_dislikes.disliker OR matcha.users.id = matha.users_dislikes.disliked)'
-								//query += ' LEFT JOIN matcha.users_blocks ON (matcha.users.id = matcha.users_blocks.blocker OR matcha.users.id = matha.users_blocks.blocked)'
-								//query += ' LEFT JOIN matcha.users_visits ON (matcha.users.id = matcha.users_visits.visitor OR matcha.users.id = matha.users_visits.visited)'
-								//query += ' LEFT JOIN matcha.users_reports ON matcha.users.id = matcha.users_reports.reported';
+								query += ' LEFT JOIN matcha.users_interests ON matcha.users.id = matcha.users_interests.user';
+								query += ' LEFT JOIN matcha.users_likes ON (matcha.users.id = matcha.users_likes.liker OR matcha.users.id = matha.users_likes.liked)'
+								query += ' LEFT JOIN matcha.users_dislikes ON (matcha.users.id = matcha.users_dislikes.disliker OR matcha.users.id = matha.users_dislikes.disliked)'
+								query += ' LEFT JOIN matcha.users_blocks ON (matcha.users.id = matcha.users_blocks.blocker OR matcha.users.id = matha.users_blocks.blocked)'
+								query += ' LEFT JOIN matcha.users_visits ON (matcha.users.id = matcha.users_visits.visitor OR matcha.users.id = matha.users_visits.visited)'
+								query += ' LEFT JOIN matcha.users_reports ON matcha.users.id = matcha.users_reports.reported';
 								query += ' WHERE matcha.users.username = ?';
 								connection.query(query, [
 									username

67a5677f596662aa143158c9a7276b71a161b2f2
Hollyol <HollyolGamu@hotmail.com>
2019-10-21 16:23:02 +0200

    Rewrite delete_user

--- memberManager.js
+++ memberManager.js
@@ -917,8 +917,8 @@ module.exports = {
 								resolve('Le pseudo et le mot de pass de correspondent pas');
 							} else {
 								let query = 'DELETE matcha.users, matcha.users_extended FROM matcha.users';
-								query += ' JOIN matcha.users_extended ON matcha.users.id = matcha.users_extended.user';
-								query += ' JOIN matcha.users_images ON matcha.users.id = matcha.users_images.user';
+								query += ' LEFT JOIN matcha.users_extended ON matcha.users.id = matcha.users_extended.user';
+								//query += ' LEFT JOIN matcha.users_images ON matcha.users.id = matcha.users_images.user';
 								//query += ' FULL OUTER JOIN matcha.users_interests ON matcha.users.id = matcha.users_interests.user';
 								//query += ' LEFT JOIN matcha.users_likes ON (matcha.users.id = matcha.users_likes.liker OR matcha.users.id = matha.users_likes.liked)'
 								//query += ' LEFT JOIN matcha.users_dislikes ON (matcha.users.id = matcha.users_dislikes.disliker OR matcha.users.id = matha.users_dislikes.disliked)'

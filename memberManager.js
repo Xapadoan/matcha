013e4db1fcb36a48cc4832b64904dd184d99f21b
Hollyol <HollyolGamu@hotmail.com>
2019-10-21 21:39:59 +0200

    See matches in home

--- memberManager.js
+++ memberManager.js
@@ -612,7 +612,7 @@ module.exports = {
 	},
 	getUserMatchs: function getUserMatchs(username) {
 		return (new Promise((resolve, reject) => {
-			connection.query('SELECT l.liked FROM matcha.users_likes l INNER JOIN matcha.users u ON u.id = l.liker WHERE u.username = ?', [
+			connection.query('SELECT u.id, u.username FROM matcha.users u WHERE u.id IN (SELECT l.liked FROM matcha.users_likes l INNER JOIN matcha.users u ON u.id = l.liker WHERE u.username = ?)', [
 				username
 			], (err, results) => {
 				if (err) {

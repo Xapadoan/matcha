7eb35d102fd93ddfea48c2844610c5708c7a7b98
Hollyol <HollyolGamu@hotmail.com>
2019-10-23 21:26:18 +0200

    Allow delete images

--- serveur.js
+++ serveur.js
@@ -268,6 +268,7 @@ app.post('/login', csrfProtection, (req, res) => {
 });
 
 app.get('/delete_image/:id', (req, res) => {
+	console.log('OK')
 	if (req.params.id < 1 || req.params.id > 5) {
 		req.session.notification = 'Cette photo n\'existe pas';
 		res.redirect(301, '/home');

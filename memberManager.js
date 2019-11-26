cedcb14bfaf5e1f619a6adc7579b95774fe7fabe
Hollyol <HollyolGamu@hotmail.com>
2019-11-26 17:59:58 +0100

    add sort options

--- memberManager.js
+++ memberManager.js
@@ -1539,7 +1539,7 @@ module.exports = {
 					console.log(err.stack);
 					reject('Failed to fetch users');
 				} else {
-					console.log(results[0].likes)
+					console.log(results[1].likes)
 					resolve(results);
 				}
 			})

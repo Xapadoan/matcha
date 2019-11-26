87066dccb4ee3f21db7e7fa55eb34906960180d6
Hollyol <HollyolGamu@hotmail.com>
2019-11-26 15:24:28 +0100

    sort searches

--- memberManager.js
+++ memberManager.js
@@ -1454,9 +1454,9 @@ module.exports = {
 	//	}
 	fetchMembers: function fetchMembers(options, fetcher) {
 		return (new Promise((resolve, reject) => {
-			query = 'SELECT u.id, u.firstname, u.lastname, u.fruit, e.age, e.gender, e.bio, i.image1, (e.lat - ?) AS distance, (SELECT COUNT(*) FROM matcha.users_likes WHERE liked = ?) AS likes, (SELECT COUNT(*) FROM matcha.users_visits WHERE visited = ?) AS visits, ((5 * likes + visits)) AS pop_score FROM matcha.users u INNER JOIN matcha.users_extended e ON u.id = e.user INNER JOIN matcha.users_images i ON u.id = i.user INNER JOIN matcha.users_interests n ON u.id = n.user'
-			query_values = [fetcher.username, fetcher.location.lat, fetcher.location.lat, fetcher.location.lng, fetcher.location.lng, fetcher.id, fetcher.id];
+			query = 'SELECT u.id, u.firstname, u.lastname, u.fruit, e.age, e.gender, e.bio, i.image1, ((e.lat - ?) * (e.lat - ?) + (e.lng - ?) * (e.lng - ?)) AS distance, (SELECT COUNT(*) FROM matcha.users_likes WHERE liked = ?) AS likes, (SELECT COUNT(*) FROM matcha.users_visits WHERE visited = ?) AS visits, ((5 * likes + visits)) AS pop_score FROM matcha.users u INNER JOIN matcha.users_extended e ON u.id = e.user INNER JOIN matcha.users_images i ON u.id = i.user INNER JOIN matcha.users_interests n ON u.id = n.user'
 			query += ' WHERE u.username <> ?';
+			query_values = [fetcher.location.lat, fetcher.location.lat, fetcher.location.lng, fetcher.location.lng, fetcher.id, fetcher.id, fetcher.username];
 			//use age
 			if (typeof options.age != 'undefined') {
 				query += ' AND e.age BETWEEN ? and ?';
